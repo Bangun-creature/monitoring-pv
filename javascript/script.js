5a3bb5522f2ae3bc0c411ff83e291f2f6de92382
Bangun Wismoyo <bangunjati03@sma.belajar.id>
2025-05-10 21:49:30 +0700

    Menambahkan halaman reports dan menambahkan animasi di file script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,22 @@
 const dashboardButton = document.getElementById("dashboard");
 const voltageButton = document.getElementById("voltage");
 const currentButton = document.getElementById("current");
-const reportButton = document.getElementById("reports");
+const reportsButton = document.getElementById("reports");
 const settingsButton = document.getElementById("settings");
 
 const dashboardField = document.getElementById("dashboard-field");
 const voltageField = document.getElementById("voltage-field");
 const currentField = document.getElementById("current-field");
+const reportsField = document.getElementById("reports-field");
 
 dashboardButton.addEventListener("click", (e) => {
   voltageField.classList.add("hidden");
   currentField.classList.add("hidden");
+  reportsField.classList.add("hidden");
 
   voltageButton.classList.remove("active");
   currentButton.classList.remove("active");
+  reportsButton.classList.remove("active");
 
   dashboardField.classList.remove("hidden");
 
@@ -23,9 +26,11 @@ dashboardButton.addEventListener("click", (e) => {
 voltageButton.addEventListener("click", () => {
   dashboardField.classList.add("hidden");
   currentField.classList.add("hidden");
+  reportsField.classList.add("hidden");
 
   dashboardButton.classList.remove("active");
   currentButton.classList.remove("active");
+  reportsButton.classList.remove("active");
 
   voltageField.classList.remove("hidden");
 
@@ -35,11 +40,27 @@ voltageButton.addEventListener("click", () => {
 currentButton.addEventListener("click", () => {
   dashboardField.classList.add("hidden");
   voltageField.classList.add("hidden");
+  reportsField.classList.add("hidden");
 
   dashboardButton.classList.remove("active");
   voltageButton.classList.remove("active");
+  reportsButton.classList.remove("active");
 
   currentField.classList.remove("hidden");
 
   currentButton.classList.add("active");
+});
+
+reportsButton.addEventListener("click", () => {
+  dashboardField.classList.add("hidden");
+  voltageField.classList.add("hidden");
+  currentField.classList.add("hidden");
+
+  dashboardButton.classList.remove("active");
+  voltageButton.classList.remove("active");
+  currentButton.classList.remove("active");
+
+  reportsField.classList.remove("hidden");
+
+  reportsButton.classList.add("active");
 });
